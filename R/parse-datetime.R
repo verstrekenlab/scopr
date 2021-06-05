7e071210ef8da9bb93f96c5241f265d2f4a92307
Antonio Ortega <ntoniohu@gmail.com>
2021-06-05 17:39:24 +0200

    Document parse-datetime

--- R/parse-datetime.R
+++ R/parse-datetime.R
@@ -1,20 +1,15 @@
-#
-# parse_date <- function(x, format="", tz="UTC"){
-#   out <- lapply(x, parse_one_date, format = format, tz = tz)
-#   do.call(c, out)
-# }
-
+#' Parse a character date into R 
+#'
+#' Expected format is "YYYY-MM-DD"
+#' @importFrom readr parse_date stop_for_problems
 parse_date <- function(x, format="", tz="UTC"){
 
-  # if(is.factor(x))
-  x <-  as.character(x)
-
-  if(any(c("POSIXct","numeric") %in% class(x)))
+  # if x is already one of these three classes, it is already parsed
+  if(any(c("POSIXct","numeric", "Date") %in% class(x)))
     return(x)
+  # if it's a character, we need to parse it
   if(is.character(x)){
-
     out <- readr::parse_date(x, format=format, locale = readr::locale(tz = tz))
-
     readr::stop_for_problems(out)
     return(out)
   }
@@ -22,13 +17,12 @@ parse_date <- function(x, format="", tz="UTC"){
 }
 
 
-# parse_time <- function(x, format="", tz="UTC"){
-#   out <- lapply(x, parse_one_time, format = format, tz = tz)
-#   do.call(c, out)
-# }
-
+#' Parse a time date into R 
+#'
+#' Expected format is "HH:MM:SS"
+#' @importFrom readr parse_time stop_for_problems
 parse_time <- function(x, format="", tz="UTC"){
-  # if(is.factor(x))
+
   x <-  as.character(x)
   if("difftime" %in% class(x))
     return(x)
