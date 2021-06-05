002e3558e47a7756bd9cf2ace75150eb00fa6737
Antonio Ortega <ntoniohu@gmail.com>
2021-06-05 17:40:02 +0200

    Typos in utils-dataframe

--- R/utils-dataframe.R
+++ R/utils-dataframe.R
@@ -1,10 +1,10 @@
-#' Checks if the expected columns are all in a given character vector. Through error if not
-#' @param expected_colnames the colnmaes that should be in a dt
+#' Checks if the expected columns are all in a given character vector. Error otherwise
+#' @param expected_colnames the colnames that should be in a dt
 #' @param df the dataframe in which the columns are expected
 #' @noRd
 check_columns <- function(expected_colnames, df){
   cols <- colnames(df)
   col_not_found <- expected_colnames[!expected_colnames %in% cols]
-  if(length(col_not_found)>0)
-    stop(sprintf("The following columns are needed, but not found: %s",col_not_found))
+  if(length(col_not_found) > 0)
+    stop("The following columns are needed, but not found: ", col_not_found)
 }
