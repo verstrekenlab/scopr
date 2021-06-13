b61b7cb300a8b94287d9e0ffaec0eb58bbffa7f2
Antonio Ortega <ntoniohu@gmail.com>
2021-06-13 12:46:40 +0200

    Make a testing metadata internal

--- inst/internal.R
+++ inst/internal.R
@@ -4,4 +4,11 @@ library(ggetho)
 library(usethis)
 
 var_map <- read.table("inst/extdata/var_map.csv", header=TRUE,sep=",")
-use_data(var_map, internal = TRUE, overwrite = TRUE)
+
+metadata <- data.table(region_id = 1:20,
+                       machine_name = c("E_014"),
+                       date = c("2016-01-25"),
+                       test=c(1)
+)
+
+use_data(var_map, metadata, internal = TRUE, overwrite = TRUE)
